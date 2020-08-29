192c6e4db0000efb4afd580a9815bd48ce895b26
Divaksh <me@divaksh.me>
2020-08-30 03:46:45 +0530

    Added post meta such as date and category to post component - Added functionality from the mars-theme - Added post date and post category - Fixed html encoding issue for the post title in mobile devices

--- packages/forgotten-developer/src/post.js
+++ packages/forgotten-developer/src/post.js
@@ -1,20 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect, styled } from "frontity";
+import Link from "./link";
+import List from "./list";
+import FeaturedMedia from "./featured-media";
 
-const Post = ({ state }) => {
+const Post = ({ state, actions, libraries }) => {
+  // Get information about the current URL.
   const data = state.source.get(state.router.link);
+  // Get the data of the post.
   const post = state.source[data.type][data.id];
+  // Get the data of the author.
+  const author = state.source.author[post.author];
+  // Get a human readable date.
+  const date = new Date(post.date);
 
-  return (
+  // Get the html2react component.
+  const Html2React = libraries.html2react.Component;
+
+  /**
+   * Once the post has loaded in the DOM, prefetch both the
+   * home posts and the list component so if the user visits
+   * the home page, everything is ready and it loads instantly.
+   */
+  useEffect(() => {
+    actions.source.fetch("/");
+    List.preload();
+  }, []);
+
+  return data.isReady ? (
     <>
-      <div>
-        <PostTitle>
-          <h2>{post.title.rendered}</h2>
-        </PostTitle>
-        <PostContainer>
-          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-        </PostContainer>
-      </div>
+      <Container>
+        <div>
+          <PostTitle
+            dangerouslySetInnerHTML={{ __html: post.title.rendered }}
+          />
+
+          {/* Only display author and date on posts */}
+          {data.isPost && (
+            <div>
+              {author && (
+                <StyledLink link={author.link}>
+                  <Author>
+                    By <b>{author.name}</b>
+                  </Author>
+                </StyledLink>
+              )}
+              <DateWrapper>
+                {" "}
+                Compiled on <b>{date.toDateString()}</b>
+              </DateWrapper>
+              <PublishIn>
+                {" "}
+                in <b>{state.source.category[post.categories[0]].name}</b>
+              </PublishIn>
+            </div>
+          )}
+        </div>
+
+        {/* Look at the settings to see if we should include the featured image */}
+        {state.theme.featured.showOnPost && (
+          <FeaturedMedia id={post.featured_media} />
+        )}
+
+        {/* Render the content using the Html2React component so the HTML is processed
+       by the processors we included in the libraries.html2react.processors array. */}
+        <Content>
+          <Html2React html={post.content.rendered} />
+        </Content>
+      </Container>
+
+      {/* Scripts for Syntax Highlighting*/}
       <script
         src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.21.0/components/prism-core.min.js"
         integrity="sha512-hqRrGU7ys5tkcqxx5FIZTBb7PkO2o3mU6U5+qB9b55kgMlBUT4J2wPwQfMCxeJW1fC8pBxuatxoH//z0FInhrA=="
@@ -26,12 +81,18 @@ const Post = ({ state }) => {
         crossorigin="anonymous"
       ></script>
     </>
-  );
+  ) : null;
 };
 
 export default connect(Post);
 
-const PostContainer = styled.div`
+const Container = styled.div``;
+
+const Author = styled.span``;
+
+const DateWrapper = styled.span``;
+
+const Content = styled.div`
   font-size: 18px !important;
   p {
     line-height: 1.6em;
@@ -40,6 +101,10 @@ const PostContainer = styled.div`
     max-width: 100%;
     height: 100%;
   }
+`;
+
+const PublishIn = styled.span`
+  font-size: 16px !important;
 `;
 
 const PostTitle = styled.h2`
