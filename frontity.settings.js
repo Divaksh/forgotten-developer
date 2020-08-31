9e8055231e4fc06ef839b97d4d12de0c931f254b
Divaksh <me@divaksh.me>
2020-08-31 12:41:05 +0530

    Added html2react library to theme index - Fixed theme in json files - Fixed url in frontity settings

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,42 +1,42 @@
 const settings = {
-  name: "divaksh.com",
-  state: {
-    frontity: {
-      url: "https://test.frontity.org",
+  "name": "divaksh.com",
+  "state": {
+    "frontity": {
+      url: "https://divaksh.com",
       title: "Divaksh",
       description: "",
-    },
+    }
   },
-  packages: [
+  "packages": [
     {
-      name: "forgotten-developer",
-      state: {
-        theme: {
-          menu: [
+      "name": "forgotten-developer",
+      "state": {
+        "theme": {
+          "menu": [
             ["Home", "/"],
             ["Java", "/category/programming/java/"],
             ["JavaScript", "/category/programming/javascript/"],
             ["C", "/category/programming/c/"],
             ["About Us", "/about-us/"],
-          ],
-          featured: {
-            showOnList: false,
-            showOnPost: false,
-          },
-        },
-      },
+			],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
     },
     {
-      name: "@frontity/wp-source",
-      state: {
-        source: {
-          api: "https://api.divaksh.com/wp-json",
-        },
-      },
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://api.divaksh.com/wp-json"
+        }
+      }
     },
     "@frontity/tiny-router",
-    "@frontity/html2react",
-  ],
+    "@frontity/html2react"
+  ]
 };
 
 export default settings;
--- packages/forgotten-developer/src/index.js
+++ packages/forgotten-developer/src/index.js
@@ -1,13 +1,20 @@
 import Root from "./Root";
+import image from "@frontity/html2react/processors/image";
+import iframe from "@frontity/html2react/processors/iframe";
 
 export default {
-  name: "terminal-black",
+  name: "forgotten-developer",
   roots: {
     theme: Root,
   },
   state: {
     theme: {
+      menu: [],
       isMenuOpen: false,
+      featured: {
+        showOnList: false,
+        showOnPost: false,
+      },
     },
   },
   actions: {
@@ -18,6 +25,15 @@ export default {
       closeMenu: ({ state }) => {
         state.theme.isMenuOpen = false;
       },
+    },
+  },
+  libraries: {
+    html2react: {
+      /**
+       * Add a processor to `html2react` so it processes the `<img>` tags
+       * inside the content HTML. You can add your own processors too
+       */
+      processors: [image, iframe],
     },
   },
 };
