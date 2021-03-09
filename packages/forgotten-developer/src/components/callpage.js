5473c5ea220207fdb452c8591bd1344ee2ef6c72
Divaksh Jain <me@divaksh.me>
2021-03-09 18:20:02 +0530

    Updated callpage widget script for pages

--- packages/forgotten-developer/src/components/callpage.js
+++ packages/forgotten-developer/src/components/callpage.js
@@ -2,7 +2,14 @@ import Script from "@frontity/components/script";
 import { connect } from "frontity";
 
 const CallPage = () => (
-  <Script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js" />
+  <Script
+    code={`
+        <!-- BEGIN callpage.io widget -->
+        <!-- IMPORTANT: Remove script below if you don't need support for older browsers. -->
+        (function () {var script = document.createElement('script');script.src = 'https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js';script.async = false;document.head.appendChild(script);}())
+        <!-- END callpage.io widget -->
+`}
+  />
 );
 
 export default connect(CallPage);
