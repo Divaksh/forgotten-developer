cd46002d18b7420767d4f7d32edb82d650be182c
Divaksh Jain <me@divaksh.me>
2021-03-09 14:23:16 +0530

    Added page support

--- packages/forgotten-developer/src/components/index.js
+++ packages/forgotten-developer/src/components/index.js
@@ -6,12 +6,12 @@ import globalStyles from "./styles/global-styles";
 import Header from "./header";
 import List from "./list/list";
 import Post from "./post";
+import Page from "./page";
 import Loading from "./loading";
 import Title from "./title";
 import PageError from "./page-error";
 import Nav from "./nav";
 import Footer from "./footer";
-
 
 /**
  * Theme is the root React component of our theme. The one we will export
@@ -23,46 +23,48 @@ const Theme = ({ state, actions }) => {
 
   return (
     <>
-    {/* Add some metatags to the <head> of the HTML. */}
+      {/* Add some metatags to the <head> of the HTML. */}
       <Title />
       <Head>
         <meta name="description" content={state.frontity.description} />
-        <html lang="en" />          
+        <html lang="en" />
       </Head>
 
-      
       {/* Add some global styles for the whole site, like body or a's. 
       Not classes here because we use CSS-in-JS. Only global HTML tags. */}
       <Global styles={globalStyles(state.theme.colors)} />
       <link
-          href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
-          rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
+        rel="stylesheet"
       ></link>
 
       {/* Styling for Syntax Highlighting*/}
-      <link rel="stylesheet" href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.min.css"
+      />
 
       {/* Main container of the site that contains everything in the page. */}
       <div id="container">
-
         {/* Add the header of the site. */}
         <Header />
 
         <div id="main">
           <Nav />
-              {/* Add the content section. It renders a different component depending
+          {/* Add the content section. It renders a different component depending
                   on the type of URL we are in. */}
-            <div id="content-container">
-              <Switch>
-                <Loading when={data.isFetching} />
-                <List when={data.isArchive} />
-                <Post when={data.isPostType} />
-                <PageError when={data.isError} />
-              </Switch>
-            </div>
+          <div id="content-container">
+            <Switch>
+              <Loading when={data.isFetching} />
+              <List when={data.isArchive} />
+              <Post when={data.isPost} />
+              <Page when={data.isPage} />
+              <PageError when={data.isError} />
+            </Switch>
+          </div>
         </div>
         <div id="footer">
-          <Footer/>
+          <Footer />
         </div>
       </div>
     </>
